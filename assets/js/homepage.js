d0194c0736d173d40c414d6cc446fe6fa7ea3b98
Jordan Ophoff <noreply>
2021-11-02 17:03:26 -0400

    set up api request

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+var getUserRepos = function(user) {
+    // format the guthub api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
+    // make a get rquest to url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+  
 getUserRepos();
