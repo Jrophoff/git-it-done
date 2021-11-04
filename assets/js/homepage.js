29f607f6f0337198b83855c54aa2fa1f3d82cef1
Jordan Ophoff <noreply>
2021-11-03 23:08:48 -0400

    ability to search for featured repos by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,10 @@
 let userFormEl = document.querySelector("#user-form");
+let languageButtonEl = document.querySelector("#language-buttons")
 let nameInputEl = document.querySelector("#username");
 let repoContainerEl = document.querySelector("#repos-container");
 let repoSearchTerm = document.querySelector("#repo-search-term");
+
+
 
 let formSubmitHandler = function (event) {
     // prevent page from refreshing
@@ -17,6 +20,16 @@ let formSubmitHandler = function (event) {
         alert("Please enter a GitHub username");
     }
 };
+
+let buttonClickHandler = function(events) {
+    let language = event.target.getAttribute("data-language")
+    
+    if (language) {
+        getFeaturedRepos(language);
+    }
+    // clear old content
+    repoContainerEl.textContent = "";
+    }
 
 var getUserRepos = function (user) {
     // format the guthub api url
@@ -36,6 +49,20 @@ var getUserRepos = function (user) {
     .catch(function(error) {
         // notice this ',catch()' getting chained onto the end of the '.then()' method
         alert("Unable to connect to GitHub");
+    });
+};
+
+let getFeaturedRepos = function(language) {
+    let apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+        } else {
+          alert("Error: GitHub User Not Found")  
+        }
     });
 };
 
@@ -87,9 +114,12 @@ let displayRepos = function (repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 
+    
+
     console.log(repos);
     console.log(searchTerm);
 };
 
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonEl.addEventListener("click", buttonClickHandler);
